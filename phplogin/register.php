50d7ec584e456f72a174e07a6972e1391e80f425
Jani_Paavilainen <janimp@metropolia.fi>
2019-10-04 21:56:40 +0300

    Pieniä korjauksia rekisteröitymiseen

--- phplogin/register.php
+++ phplogin/register.php
@@ -7,20 +7,22 @@
  */
 session_start();
 
+$username = "";
+$errors = array();
+
 $con = mysqli_connect('localhost', 'root', 'password', 'testidatabase');
 if ( mysqli_connect_errno() ) {
     die ('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 if (isset($_POST['registerUser'])) {
-// receive all input values from the form
+// Formeista haetut käyttäjä datat.
 $username = mysqli_real_escape_string($con, $_POST['username']);
 $password1 = mysqli_real_escape_string($con, $_POST['password1']);
 $password2 = mysqli_real_escape_string($con, $_POST['password2']);
 $city = mysqli_real_escape_string($con, $_POST['city']);
 
-// form validation: ensure that the form is correctly filled ...
-// by adding (array_push()) corresponding error unto $errors array
+// Tarkistetaan onko käyttäjä antanut tyhjiä arvoja.
 if (empty($username)) {
     array_push($errors, "Username is required");
 }
@@ -35,12 +37,13 @@ $user_check_query = "SELECT * FROM userstable WHERE username='$username'";
 $result = mysqli_query($con, $user_check_query);
 $user = mysqli_fetch_assoc($result);
 
-if ($user) { // if user exists
+//Tarkistetaan löytyykö tietokannasta jo kyseinen käyttäjä nimi.
+if ($user) {
     if ($user['username'] === $username) {
         array_push($errors, "Username already exists");
     }
 }
-    if (count($errors) == 0) {
+    if (count($errors) == 0) { //jos ei löydy virheitä niin lähetetään tietokantaan käyttäjän data.
 
         $query = "INSERT INTO userstable (username, password, city) 
   			  VALUES('$username', '$password1', '$city')";
